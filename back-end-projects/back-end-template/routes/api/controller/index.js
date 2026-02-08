6409b50f4c252833a11ff8d11d6762c3730e071a
labram13 <labram@uw.edu>
2026-02-07 22:06:15 -0800

    added access tokens to template

--- back-end-projects/back-end-template/routes/api/controller/index.js
+++ back-end-projects/back-end-template/routes/api/controller/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const posts = [
+  {
+    username: 'Kyle',
+    title: 'Post 1'
+  },
+  {
+    username: 'Jim',
+    title: 'Post 2'
+  }
+]
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index');
+router.get('/', authenticateToken, (req, res) => {
+  console.log(req.user)
+  res.json(posts.filter(post => post.username === req.user.name));
 });
+
+router.post('/login', (req, res) => {
+  const username = req.body.username
+  console.log(username)
+  const user =  { name: username }
+  console.log(user)
+
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({accessToken: accessToken})
+
+
+})
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) {
+    return res.sendStatus(401)
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      res.status(403)
+    }
+    req.user = user
+    next()
+  })
+
+}
 
 module.exports = router;
