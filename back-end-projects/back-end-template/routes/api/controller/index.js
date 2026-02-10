29dd2b0ddc053b6fac9853fc60c4268687b4956f
labram13 <labram@uw.edu>
2026-02-09 21:42:34 -0800

    added refresh tokens and delete to template"

--- back-end-projects/back-end-template/routes/api/controller/index.js
+++ back-end-projects/back-end-template/routes/api/controller/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 
+const authenticateToken = require('../../../middleware/auth.js')
+
 const posts = [
   {
     username: 'Kyle',
@@ -20,34 +22,6 @@ router.get('/', authenticateToken, (req, res) => {
   res.json(posts.filter(post => post.username === req.user.name));
 });
 
-router.post('/login', (req, res) => {
-  const username = req.body.username
-  console.log(username)
-  const user =  { name: username }
-  console.log(user)
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json({accessToken: accessToken})
-
-
-})
-
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-
-  if (token == null) {
-    return res.sendStatus(401)
-  }
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      res.status(403)
-    }
-    req.user = user
-    next()
-  })
-
-}
 
 module.exports = router;
