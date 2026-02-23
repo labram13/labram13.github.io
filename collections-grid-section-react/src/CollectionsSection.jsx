01800389d73786572ca00ab38b16c89ea3ec8c10
labram13 <labram@uw.edu>
2026-02-22 18:11:31 -0800

    added keys to props for li items

--- collections-grid-section-react/src/CollectionsSection.jsx
+++ collections-grid-section-react/src/CollectionsSection.jsx
@@ -8,31 +8,31 @@ function CollectionItems(props) {
 
     useEffect(() => {
 
-        const retrieveCollections = async () => {
-            const data = await fetch('https://www.greatfrontend.com/api/projects/challenges/e-commerce/collections')
-            const dataJson = await data.json();
+        // const retrieveCollections = async () => {
+        //     const data = await fetch('https://www.greatfrontend.com/api/projects/challenges/e-commerce/collections')
+        //     const dataJson = await data.json();
 
-            setItems(dataJson.data)
+        //     setItems(dataJson.data)
             
-        }
+        // }
       
-        retrieveCollections();
+        // retrieveCollections();
+   
+
+        fetch('https://www.greatfrontend.com/api/projects/challenges/e-commerce/collections')
+            .then(response => response.json())
+            .then(data => setItems(data.data))
+    
+    
+
 
 
 
     }, [])
 
 
- 
-    // useEffect(() => {
-    //     const collectionItems = items.map((item) => (
-    //         console.log(item)
-    //     ))
-    // }, [items])
-
-
     const collectionItems = items.map((item, index) => 
-        <li className={"item" + (index + 1)} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(${item.image_url})`}}>
+        <li key={item.collection_id} className={"item" + (index + 1)} style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.25)), url(${item.image_url})`}}>
             <h4>{item.name}</h4>
             <p>{item.description}</p>
         </li>
@@ -49,18 +49,6 @@ function CollectionItems(props) {
 
     return (
         <>
-        {/* <li className="item1">
-            <h4>Cozy Comforttt</h4>
-            <p>Plush fabrics and soothing designs</p>
-        </li>
-        <li className="item2">
-            <h4>test1</h4>
-            <p>test2</p>
-        </li>
-        <li className="item3">
-            <h4>test1</h4>
-            <p>test2</p>
-        </li> */}
         {collectionItems}
         </>
     
