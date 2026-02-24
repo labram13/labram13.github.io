8d2146648aed790f1bb85b3b3b7127afd87ae7f5
labram13 <labram@uw.edu>
2026-02-23 22:36:29 -0800

    started form styling and layout for mobile

---
+++ form-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Form from './Form'
+
+
+function App() {
+
+  return (
+    <main>
+      <section className="container">
+        <Form />
+      </section>
+    </main>
+  )
+}
+
+export default App
---
+++ form-react/src/Form.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Form () {
+
+    return (
+        
+        <form>
+            <div className="row">
+                <label>Email</label>
+                <input type="text" id="email1" />
+            </div>
+            <div className="row">
+                <label>Email</label>
+                <input type="text" id="email1" />
+            </div>
+            <div className="row">
+                <label>Email</label>
+                <input type="text" id="email1" />
+            </div>
+            <div className="row">
+                <label>Email</label>
+                <input type="text" id="email1" />
+            </div>
+
+        </form>
+           
+
+
+    )
+}
+
+export default Form;
