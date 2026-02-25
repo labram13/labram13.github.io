d9ef522435c4ed1bf67636de8bdb3acf27d0982c
labram13 <labram@uw.edu>
2026-02-24 16:25:55 -0800

    changed color for error message

--- form-react/src/Form.jsx
+++ form-react/src/Form.jsx
@@ -8,18 +8,25 @@ function Form () {
             <div className="row">
                 <label>Email</label>
                 <input type="text" id="email1" />
+                <p>This is a hint text</p>
             </div>
             <div className="row">
                 <label>Email</label>
                 <input type="text" id="email1" />
+                <p>This is a hint text</p>
+
             </div>
             <div className="row">
                 <label>Email</label>
                 <input type="text" id="email1" />
+                <p>This is a hint text</p>
+
             </div>
-            <div className="row">
+            <div className="row4">
                 <label>Email</label>
                 <input type="text" id="email1" />
+                <p>This is an error message</p>
+
             </div>
 
         </form>
